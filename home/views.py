ebc903c9c2745ec79ddb73532073d231b2e82d32
Aditya-aot <aditya.aot.11@gmail.com>
2022-09-02 14:08:58 +0530

    backend base

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from itertools import product
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import category_model , product_model
+import razorpay
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
@@ -18,20 +21,71 @@ def home_view(request) :
     return render(request, 'home/home.html',context)
 
 
+
+@csrf_exempt
 def detail_view(request,pk) :
     product = product_model.objects.all()
     product_info = product.filter(id=pk)
-    context = {
+    price = product_info.values("price") 
+    print(price[0])
+    #  payment with Razorpay
+    if request.method == 'POST' :
+        client = razorpay.Client(auth=("rzp_test_MAGS2JFcBu5vr1", "ZWueXiUCqEgLzdgln0Q7OqgM"))
+
+        DATA = {
+            "amount": 1000 ,
+            "currency": "INR",
+            "receipt": "receipt#1",
+            "notes": {
+                "key1": "value3",
+                "key2": "value2"
+            }
+        }
+        payment = client.order.create(data=DATA)
+
+        context = {
         'product' : product_info ,
-    }
-    return render(request, 'home/detail_product.html',context)
+        'payment' : payment,
+            }
+        print("**********************************")
+        print("**********************************")
+        print(payment , payment['id'])
+        print("**********************************")
+        print("**********************************")
+        # return render(request, 'home/detail_product.html',context)
+        return redirect("/download_confirm/"+payment['id']+"/"+pk)
+
+    #  payment with Razorpay
+    else :
+        print("**********************************")
+        print("**********************************")
+        # print(payment)
+        print("**********************************")
+        print("**********************************")
+        context = {
+            'product' : product_info ,
+        }
+        return render(request, 'home/detail_product.html',context)
 
 
 def product_page_view(request) :
     product = product_model.objects.all()
 
+
     context = {
         'product' : product[::-1] ,
     }
     return render(request, 'home/product_page.html',context)
+
+
+@csrf_exempt
+def download_confirm_view(request,order,pk) :
+    product = product_model.objects.all()
+    product_download = product.filter(id=pk)
+
+
+    context = {
+        'product' : product_download ,
+    }
+    return render(request, 'home/download_confirm.html',context)
 
